ff57a8eba30036c2f643032fce156563dadb511d
MikeKutz <mkutz1492@gmail.com>
2024-02-26 09:09:53 -0500

    Delete UDTs/ddlt_UDT_types.sql

--- UDTs/ddlt_UDT_types.sql
+++
@@ -1,9 +0,0 @@
-create type tokens_t is object ( match# int, match_class varchar2(50), rn int, token varchar2(50) );
-/
-
-create type tokens_nt is table of tokens_t;
-/
-
-create global temporary table ddlt_tokens_temp of tokens_t;
-create global temporary table ddlt_matched_tokens_temp of tokens_t;
-
